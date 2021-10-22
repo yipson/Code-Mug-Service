e106a76f6a406df4ed5f223bd3665112713e3d4a
Sebastian Valencia <sebastianvalencia.ceron@gmail.com>
2021-10-21 21:11:53 -0500

    externalizacion de credenciales de firebase

---
+++ config/firebase/code-mug-9e7de-firebase-adminsdk-4xmef-d80e6a4f16.js
@@ -0,0 +1,14 @@
+module.exports = {
+  firebaeConfig : {
+    "type": process.env.FIREBASE_CONFIG_TYPE,
+    "project_id": process.env.FIREBASE_CONFIG_PROJECT_ID,
+    "private_key_id": process.env.FIREBASE_CONFIG_PRIVATE_KEY_ID,
+    "private_key": process.env.FIREBASE_CONFIG_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    "client_email": process.env.FIREBASE_CONFIG_CLIENT_EMAIL,
+    "client_id": process.env.FIREBASE_CONFIG_CLIENT_ID,
+    "auth_uri": process.env.FIREBASE_CONFIG_AUTH_URI,
+    "token_uri": process.env.FIREBASE_CONFIG_TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.FIREBASE_CONFIG_AUTH_PROVIDER_X509_CERT_URL,
+    "client_x509_cert_url": process.env.FIREBASE_CONFIG_CLIENT_X509_CERT_URL
+  }
+}
